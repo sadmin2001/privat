5e0a61576e0e528330a07bf0e02bfc45b26c8b37
SADMIN <sadmin@whodafuq.ru>
2017-07-17 19:04:10 -0700

    Create July17thlab.cpp

---
+++ July17thlab.cpp
@@ -0,0 +1,11 @@
+#include <iostream>#include <fstream>
+using namespace std;void set_numbers(int, int, int, int);int find_max_number(int, int, int int);void create_scale(int&, int&, int&, int&);void write_header ();void create_bar(int y, int width, int height, string color);void write_footer();int number_1 = 0, number_2 = 0, number_3 = 0, number_4 = 0, max_number = 1 width = 0, height = 0, y = 0;
+
+int main(){void set_numbers(number_1&, number_2&, number_3&, number_4&);max_number = find_max_number(number_1, number_2, number_3, number_4);void create_scale();void write_header();void create_bar();void write_footer();
+    return 0;}void set_numbers(int&, int&, int&, int&);{    cout << "Please input four non-negative numbers to graph."'    cin >> number_1 >> number_2 >> number_3 >> number_4;    if ((number_1 < 0)||(number_2 < 0)||(number_3 < 0)||(number_4 < 0)){        cout << "Please input VALID non negative numbers.";         cin >> number_1 >> number_2 >> number_3 >> number_4;}         cout << "The numbers you selected are \n" << number_1 << ", " << number_2 << ", " << number_3 << ", " << number_4;    }
+}
+int find_max_number(int, int, int int);{}
+void create_scale(int&, int&, int&, int&);{}
+void write_header ();{}
+void create_bar(int y, int width, int height, string color);{}
+void write_footer();{}
